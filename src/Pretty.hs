04158dfa24ecd54a5e2dec6461f24b7bba158348
Ting-Gian LUA <banacorn@gmail.com>
2019-12-19 17:46:26 +0800

    [ new ] Fiddling with variadic monad

--- src/Pretty.hs
+++ src/Pretty.hs
@@ -1,11 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE TypeSynonymInstances, FlexibleInstances #-}
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module Pretty where
 
 import Data.Text.Prettyprint.Doc
+import Control.Monad ((>=>))
 import Control.Monad.Free
 import Prelude hiding (Ordering(..))
 
@@ -16,7 +17,21 @@ import Syntax.Lasagne
 --------------------------------------------------------------------------------
 -- | Expr
 
-data Variadic doc = Expect (Expr -> Variadic doc) | Complete doc
+data VarArg a = Expect (Expr -> VarArg a) | Complete a
+  deriving (Functor)
+
+instance Applicative VarArg where
+  pure = Complete
+  Complete f  <*> Complete x = Complete (f x)
+  Expect   f  <*> Complete x = Expect (\arg -> f arg  <*> pure x)
+  Complete f  <*> Expect   g = Expect (\arg -> pure f <*> g arg)
+  Expect   f  <*> Expect   g = Expect (\arg -> f arg  <*> g arg)
+
+instance Monad VarArg where
+  return = Complete
+  Complete x >>= f = f x
+  Expect   g >>= f = Expect (g >=> f)
+
 data Fixity = Infix Int | InfixR Int | InfixL Int | Prefix Int | Postfix Int
   deriving (Show, Eq)
 
@@ -40,36 +55,52 @@ parensIf n m
   | n > m     = parens
   | otherwise = id
 
-handleOp :: Int -> Op -> Variadic (Doc ann)
-handleOp n op = case classify op of
-  Infix m -> Expect $ \p -> Expect $ \q -> Complete $ parensIf n m $
-    prettyPrec (succ m) p
-    <+> pretty op
-    <+> prettyPrec (succ m) q
-  InfixL m -> Expect $ \p -> Expect $ \q -> Complete $ parensIf n m $
-    prettyPrec m p
-    <+> pretty op
-    <+> prettyPrec (succ m) q
-  InfixR m -> Expect $ \p -> Expect $ \q -> Complete $ parensIf n m $
-    prettyPrec (succ m) p
-    <+> pretty op
-    <+> prettyPrec m q
-  Prefix m -> Expect $ \p -> Complete $ parensIf n m $
-    pretty op
-    <+> prettyPrec m p
-  Postfix m -> Expect $ \p -> Complete $ parensIf n m $
-    prettyPrec m p
-    <+> pretty op
+var :: VarArg Expr
+var = Expect Complete
 
-handleExpr :: Int -> Expr -> Variadic (Doc ann)
-handleExpr _ (Var var) = Complete $ pretty var
-handleExpr _ (Const con) = Complete $ pretty con
-handleExpr _ (Lit lit) = Complete $ pretty lit
+handleOp :: Int -> Op -> VarArg (Doc ann)
+handleOp n op = case classify op of
+  Infix m -> do
+    p <- var
+    q <- var
+    return $ parensIf n m $
+      prettyPrec (succ m) p
+      <+> pretty op
+      <+> prettyPrec (succ m) q
+  InfixL m -> do
+    p <- var
+    q <- var
+    return $ parensIf n m $
+      prettyPrec m p
+      <+> pretty op
+      <+> prettyPrec (succ m) q
+  InfixR m -> do
+    p <- var
+    q <- var
+    return $ parensIf n m $
+      prettyPrec (succ m) p
+      <+> pretty op
+      <+> prettyPrec m q
+  Prefix m -> do
+    p <- var
+    return $ parensIf n m $
+      pretty op
+      <+> prettyPrec m p
+  Postfix m -> do
+    p <- var
+    return $ parensIf n m $
+      prettyPrec m p
+      <+> pretty op
+
+handleExpr :: Int -> Expr -> VarArg (Doc ann)
+handleExpr _ (Var v) = return $ pretty v
+handleExpr _ (Const c) = return $ pretty c
+handleExpr _ (Lit lit) = return $ pretty lit
 handleExpr n (Op op) = handleOp n op
 handleExpr n (App p q) = case handleExpr n p of
-  Expect f   -> f(q)
+  Expect f -> f q
   Complete s -> Complete s
-handleExpr _ (Hole i _) = Complete $ "[" <> pretty i <> "]"
+handleExpr _ (Hole i _) = return $ "[" <> pretty i <> "]"
 
 prettyPrec :: Int -> Expr -> Doc ann
 prettyPrec n expr = case handleExpr n expr of
