3bf09275156a66ca0ca78f2bef28f7c0ab2079bc
Shin-Cheng Mu <scm@iis.sinica.edu.tw>
2020-01-13 18:05:29 +0800

    [new] ObliOrigin. Tracing where an obligation came from.

--- src/Pretty/GCL/WP.hs
+++ src/Pretty/GCL/WP.hs
@@ -11,9 +11,10 @@ import Pretty.Abstract.Simple ()
 -- | Obligation & Specification
 
 instance Pretty Obligation where
-  pretty (Obligation i p q) = lbracket <> pretty i <> rbracket <+> line <>
+  pretty (Obligation i p q os) = lbracket <> pretty i <> rbracket <+> line <>
     indent 2 (pretty p) <> line <>
-    indent 2 (pretty q) <> line
+    indent 2 (pretty q) <> line <>
+    indent 2 (pretty (show os)) <> line
 
 instance Pretty Specification where
   pretty (Specification i p q _) = lbracket <> pretty i <> rbracket <> line <>
