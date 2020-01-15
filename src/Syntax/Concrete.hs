3e71e408636f111d395f10284ab32bca8fcd3724
Ting-Gian LUA <banacorn@gmail.com>
2020-01-15 13:21:27 +0800

    [ new ] Substitution for Concrete

--- src/Syntax/Concrete.hs
+++ src/Syntax/Concrete.hs
@@ -1,10 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Syntax.Concrete
   ( module Syntax.Concrete
   , Op(..), TBase(..), Lit(..)  -- re-exporting from Syntax.Abstract
   ) where
 
 import Data.Loc
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Set (Set, (\\))
+import qualified Data.Set as Set
 import Data.Text.Lazy (Text)
+import qualified Data.Text.Lazy as Text
 import Prelude hiding (Ordering(..))
 
 import Syntax.Abstract (Op(..), TBase(..), Lit(..))
@@ -70,11 +77,137 @@ data Expr = Lit   Lit       Loc
 data Upper = Upper Text Loc
   deriving (Eq, Show)
 
+instance Ord Upper where
+  compare (Upper a _) (Upper b _) = compare a b
+
 data Lower = Lower Text Loc
   deriving (Eq, Show)
+
+instance Ord Lower where
+  compare (Lower a _) (Lower b _) = compare a b
 
 upperToText :: Upper -> Text
 upperToText (Upper x _) = x
 
 lowerToText :: Lower -> Text
 lowerToText (Lower x _) = x
+
+--------------------------------------------------------------------------------
+-- | Helpers
+
+binary :: Op -> Expr -> Expr -> Expr
+binary op x y = App (App (Op op NoLoc) x NoLoc) y (x <--> y)
+
+lt, gt, gte, lte, eqq, conj, disj, implies :: Expr -> Expr -> Expr
+lt = binary LT
+gt = binary GT
+gte = binary GTE
+lte = binary LTE
+eqq = binary EQ
+conj = binary Conj
+disj = binary Disj
+implies = binary Implies
+
+true :: Expr
+true = Lit (Bol True) NoLoc
+
+false :: Expr
+false = Lit (Bol False) NoLoc
+
+conjunct :: [Expr] -> Expr
+conjunct []     = true
+conjunct [p]    = p
+conjunct (p:ps) = p `conj` conjunct ps
+
+disjunct :: [Expr] -> Expr
+disjunct []     = false
+disjunct [p]    = p
+disjunct (p:ps) = p `disj` disjunct ps
+
+--------------------------------------------------------------------------------
+-- | Instance of Located
+
+instance Located Expr where
+  locOf (Var _ l)   = l
+  locOf (Const _ l) = l
+  locOf (Lit _ l)   = l
+  locOf (Op _ l)    = l
+  locOf (App _ _ l) = l
+  locOf (Hole l)    = l
+  locOf (Quant _ _ _ _ l) = l
+
+
+--------------------------------------------------------------------------------
+-- | Substitution
+
+type Name = Either Lower Upper
+type Subst = Map Name Expr
+
+type Var = Lower
+
+
+  -- SCM: substituion needs fresh names. However, I don't
+  --      want to move M into this module. Therefore I am
+  --      using a type class.
+
+class Monad m => Fresh m where
+  fresh :: m Int
+  freshVar :: Text -> m Var
+  freshVar prefix = do
+    i <- fresh
+    let name = "_" <> prefix <> Text.pack (show i)
+    return $ Lower name NoLoc
+
+
+  freshVars :: Text -> Int -> m [Var]
+  freshVars _  0 = return []
+  freshVars pf n = (:) <$> freshVar pf <*> freshVars pf (n-1)
+
+subst :: Fresh m => Subst -> Expr -> m Expr
+subst env (Var x l)     = return $ maybe (Var x l) id (Map.lookup (Left x) env)
+subst env (Const x l)   = return $ maybe (Const x l) id (Map.lookup (Right x) env)
+subst _   (Op op l)     = return $ Op op l
+subst _   (Lit n l)     = return $ Lit n l
+subst env (App e1 e2 l) = App <$> subst env e1 <*> subst env e2 <*> pure l
+subst env (Hole      l) = return $ Hole l
+-- subst env (Hole      l) = return $ Hole 0 [env]
+-- subst env (Hole idx subs l) = return $ Hole idx (env:subs)
+subst env (Quant op xs range term l) = do
+   op' <- subst env op
+   (xs', range', term') <- subLocal xs range term
+
+   let env' = Map.filterWithKey (\k _ -> case k of
+                  Left k' -> not (k' `elem` xs')
+                  Right _ -> False) env
+
+   Quant op' xs' <$> subst env' range' <*> subst env' term' <*> pure l
+
+  where subLocal :: Fresh m => [Var] -> Expr -> Expr -> m ([Var], Expr, Expr)
+        subLocal []     r t = return ([],r,t)
+        subLocal (i:is) r t
+          | Left i `elem` freeInEnv = do
+              -- if `i` is a free variable of `env`
+              -- instantiate a dummy varialbe `j`
+              j <- freshVar "dummy"
+              -- substitute the variable `i` in `r` with the dummy variable
+              r' <- subst (Map.singleton (Left i) (Var j NoLoc)) r
+              -- substitute the variable `i` in `t` with the dummy variable
+              t' <- subst (Map.singleton (Left i) (Var j NoLoc)) t
+              first3 (j:) <$> subLocal is r' t'
+          | otherwise = first3 (i:) <$> subLocal is r t
+        freeInEnv = freeSubst env
+        first3 f (x,y,z) = (f x, y, z)
+
+free :: Expr -> Set Name
+free (Var x _)     = Set.singleton (Left x)
+free (Const x _)   = Set.singleton (Right x)
+free (Op _ _)      = mempty
+free (Lit _ _)     = mempty
+free (App e1 e2 _) = free e1 <> free e2  -- not worrying about duplication
+free (Quant op xs range term _) = (free op <> free range <> free term) \\ Set.fromList (map Left xs)
+free (Hole _) = mempty -- banacorn: `subs` has been always empty anyway
+    -- concat (map freeSubst subs) -- correct?
+
+-- free variables in the Subst table
+freeSubst :: Subst -> Set Name
+freeSubst = Set.unions . map free . Map.elems
