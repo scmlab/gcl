0840cf1432f73524d703e0a73cd5ec08515f5dfa
Ting-Gian LUA <banacorn@gmail.com>
2020-02-12 15:38:57 +0800

    [ refactor ] Test

--- test/Test/WP.hs
+++ test/Test/WP.hs
@@ -27,10 +27,7 @@ tests = testGroup "Weakest Precondition"
 simple :: TestTree
 simple = testGroup "simple program"
   [ testCase "skip" $ run "skip" @?= Right
-      (
-        Assertion
-          (Var (Lower "false" (pos 2 3 7 <--> pos 2 7 11)) (pos 2 3 7 <--> pos 2 7 11))
-          (pos 2 1 5 <--> pos 2 9 13)
+      ( assertFalse 1 4
       , []
       , []
       )
@@ -48,32 +45,42 @@ simple = testGroup "simple program"
       , []
       )
   , testCase "assignment" $ run "x := 1" @?= Right
-      ( Assertion
-          (Var (Lower "false" (pos 2 3 9 <--> pos 2 7 13)) (pos 2 3 9 <--> pos 2 7 13))
-          (pos 2 1 7 <--> pos 2 9 15)
+      ( assertFalse 1 6
       , []
       , []
       )
-  , testCase "spec" $ run "{!\n!}\n" @?= Right
-      ( Assertion
-          (Var (Lower "false" (pos 4 3 9 <--> pos 4 7 13)) (pos 4 3 9 <--> pos 4 7 13))
-          (pos 4 1 7 <--> pos 4 9 15)
+  , testCase "spec" $ run "{!\n!}" @?= Right
+      ( assertFalse 2 5
       , []
       , [ Specification
             0
-            (Assertion
-              (Var (Lower "false" (pos 4 3 9 <--> pos 4 7 13)) (pos 4 3 9 <--> pos 4 7 13))
-              (pos 4 1 7 <--> pos 4 9 15))
-            (Assertion
-              (Var (Lower "false" (pos 4 3 9 <--> pos 4 7 13)) (pos 4 3 9 <--> pos 4 7 13))
-              (pos 4 1 7 <--> pos 4 9 15))
+            (assertFalse 2 5)
+            (assertFalse 2 5)
             (pos 1 1 0 <--> pos 2 2 4)
         ]
       )
   ]
   where
+    -- assertTrue :: Int -> Int -> Pred
+    -- assertTrue rowBefore charBefore =
+    --   Assertion
+    --     (Lit (Bol False) (pos lineNo 3 (colNo + 2) <--> pos lineNo 6 (colNo + 5)))
+    --     (pos lineNo 1 colNo <--> pos lineNo 8 (colNo + 7))
+    --   where
+    --     lineNo = rowBefore + 1
+    --     colNo = charBefore + 1
+
+    assertFalse :: Int -> Int -> Pred
+    assertFalse rowBefore charBefore =
+      Assertion
+        (Lit (Bol False) (pos lineNo 3 (colNo + 2) <--> pos lineNo 7 (colNo + 6)))
+        (pos lineNo 1 colNo <--> pos lineNo 9 (colNo + 8))
+      where
+        lineNo = rowBefore + 1
+        colNo = charBefore + 1
+
     run :: Text -> Either [Error] (Pred, [Obligation], [Specification])
-    run text = REPL.scan "<test>" (text <> "\n{ false }")
+    run text = REPL.scan "<test>" (text <> "\n{ False }")
                 >>= REPL.parseProgram "<test>"
                 >>= REPL.sweep
 
