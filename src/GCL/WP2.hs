08dbea2ccc5df0455327dbafc2dd6c38cd208d4d
Ting-Gian LUA <banacorn@gmail.com>
2020-03-19 13:09:07 +0800

    [ dep ] Bump LTS to 15.3

--- src/GCL/WP2.hs
+++ src/GCL/WP2.hs
@@ -1,33 +1,38 @@
-{-# LANGUAGE OverloadedStrings, FlexibleContexts, DeriveGeneric,
-             TypeSynonymInstances, FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings, FlexibleContexts, DeriveGeneric, FlexibleInstances #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
+
 
 module GCL.WP2 where
 
-import Control.Monad.State hiding (guard)
-import Control.Monad.Writer hiding (guard)
-import Control.Monad.Except hiding (guard)
+import           Control.Monad.State     hiding ( guard )
+import           Control.Monad.Writer    hiding ( guard )
+import           Control.Monad.Except    hiding ( guard )
 
-import qualified Data.Map as Map
-import Data.Loc (Loc(..), Located(..), L(..))
-import Data.Aeson
-import GHC.Generics
+import qualified Data.Map                      as Map
+import           Data.Loc                       ( Loc(..)
+                                                , Located(..)
+                                                , L(..)
+                                                )
+import           Data.Aeson
+import           GHC.Generics
 
-import Syntax.Concrete (Expr, Lower)
-import qualified Syntax.Concrete as C
+import           Syntax.Concrete                ( Expr
+                                                , Lower
+                                                )
+import qualified Syntax.Concrete               as C
 -- import qualified Syntax.Predicate as P
-import Syntax.Predicate
-import Syntax.Location (ToNoLoc(..))
+import           Syntax.Predicate
+import           Syntax.Location                ( ToNoLoc(..) )
 
-import Pretty.Concrete ()
-import Pretty.Predicate ()
+import           Pretty.Concrete                ( )
+import           Pretty.Predicate               ( )
 
 sweep :: C.Program -> Either StructError2 ([PO], [Spec])
 sweep program = runWPM $ do
   struct <- programToStruct program
 
-  pos <- runPOM $ genPO struct
-  specs <- runSpecM $ genSpec struct
+  pos    <- runPOM $ genPO struct
+  specs  <- runSpecM $ genSpec struct
 
   return (pos, specs)
 
@@ -64,30 +69,30 @@ data Origin = AtAbort           Loc
             deriving (Eq, Show, Generic)
 
 instance ToNoLoc Origin where
-  toNoLoc (AtAbort          _)  = AtAbort NoLoc
-  toNoLoc (AtSkip           _)  = AtSkip NoLoc
-  toNoLoc (AtSpec           _)  = AtSpec NoLoc
-  toNoLoc (AtAssignment     _)  = AtAssignment NoLoc
-  toNoLoc (AtAssertion      _)  = AtAssertion NoLoc
-  toNoLoc (AtLoopInvariant  _)  = AtLoopInvariant NoLoc
-  toNoLoc (AtIf             _)  = AtIf NoLoc
-  toNoLoc (AtLoop           _)  = AtLoop NoLoc
-  toNoLoc (AtTermination    _)  = AtTermination NoLoc
-  toNoLoc (AtBoundDecrement _)  = AtBoundDecrement NoLoc
+  toNoLoc (AtAbort          _) = AtAbort NoLoc
+  toNoLoc (AtSkip           _) = AtSkip NoLoc
+  toNoLoc (AtSpec           _) = AtSpec NoLoc
+  toNoLoc (AtAssignment     _) = AtAssignment NoLoc
+  toNoLoc (AtAssertion      _) = AtAssertion NoLoc
+  toNoLoc (AtLoopInvariant  _) = AtLoopInvariant NoLoc
+  toNoLoc (AtIf             _) = AtIf NoLoc
+  toNoLoc (AtLoop           _) = AtLoop NoLoc
+  toNoLoc (AtTermination    _) = AtTermination NoLoc
+  toNoLoc (AtBoundDecrement _) = AtBoundDecrement NoLoc
 
 originOfStmt :: Stmt -> Origin
-originOfStmt (Abort  l) = AtAbort (locOf l)
-originOfStmt (Skip   l) = AtSkip (locOf l)
+originOfStmt (Abort l     ) = AtAbort (locOf l)
+originOfStmt (Skip  l     ) = AtSkip (locOf l)
 originOfStmt (Assign l _ _) = AtAssignment (locOf l)
-originOfStmt (If l _  ) = AtIf (locOf l)
-originOfStmt (Do l _ _) = AtLoop (locOf l)
-originOfStmt (Spec l _) = AtSpec (locOf l)
+originOfStmt (If l _      ) = AtIf (locOf l)
+originOfStmt (Do l _ _    ) = AtLoop (locOf l)
+originOfStmt (Spec l _    ) = AtSpec (locOf l)
 
 -- get the Origin of the first statement in a Struct
 originOfStruct :: Struct -> Origin
-originOfStruct (Struct _ []       next) = AtAssertion (locOf $ extractAssertion next)
-originOfStruct (Struct _ (stmt:_)    _) = originOfStmt stmt
-originOfStruct (Postcond p)             = AtAssertion (locOf p)
+originOfStruct (Struct _ [] next) = AtAssertion (locOf $ extractAssertion next)
+originOfStruct (Struct _ (stmt : _) _) = originOfStmt stmt
+originOfStruct (Postcond p) = AtAssertion (locOf p)
 
 --------------------------------------------------------------------------------
 -- | Obligation
@@ -97,8 +102,7 @@ data PO
   deriving (Eq, Show, Generic)
 
 instance ToNoLoc PO where
-  toNoLoc (PO i p q o) =
-    PO i (toNoLoc p) (toNoLoc q) (toNoLoc o)
+  toNoLoc (PO i p q o) = PO i (toNoLoc p) (toNoLoc q) (toNoLoc o)
 
 -- Monad on top of WPM, for generating proof obligations
 type POM = WriterT [PO] (StateT Int WPM)
@@ -113,58 +117,54 @@ runPOM :: POM a -> WPM [PO]
 runPOM f = evalStateT (execWriterT f) 0
 
 tellPO :: Pred -> Pred -> Origin -> POM ()
-tellPO p q l = do
+tellPO p q l = unless (C.predEq (toExpr p) (toExpr q)) $ do
   -- NOTE: this could use some love
-  unless (C.predEq (toExpr p) (toExpr q)) $ do
-    i <- get
-    put (succ i)
-    tell [PO i p q l]
+  i <- get
+  put (succ i)
+  tell [PO i p q l]
 
 genPO :: Struct -> POM ()
-genPO (Postcond _) = return ()
+genPO (Postcond _        ) = return ()
 genPO (Struct pre [] next) = do
-  tellPO pre (extractAssertion next) (AtAssertion (locOf $ extractAssertion next))
+  tellPO pre
+         (extractAssertion next)
+         (AtAssertion (locOf $ extractAssertion next))
   genPO next
-genPO (Struct pre (stmt:stmts) next) = do
+genPO (Struct pre (stmt : stmts) next) = do
   tellPO pre (precond stmt) (originOfStmt stmt)
 
   case stmt of
-    If _ gdCmds -> do
       -- inductive case
-      mapM_ (genPO . gdCmdBody) gdCmds
+    If _ gdCmds     -> mapM_ (genPO . gdCmdBody) gdCmds
     Do l bnd gdCmds -> do
-      let loc = locOf l
-      let guards = map gdCmdGuard gdCmds
+      let loc           = locOf l
+      let guards        = map gdCmdGuard gdCmds
       -- the loop invariant has already been stored in Struct
       let loopInvariant = pre
       -- post condition of the current DO statement
       let post = case stmts of
-                  []    -> extractAssertion next
-                  (x:_) -> precond x
+            []      -> extractAssertion next
+            (x : _) -> precond x
 
       -- base case
-      tellPO
-        (conjunct (loopInvariant : map Negate guards))
-        post
-        (AtLoop loc)
+      tellPO (conjunct (loopInvariant : map Negate guards)) post (AtLoop loc)
 
       -- inductive case
       mapM_ (genPO . gdCmdBody) gdCmds
 
       -- termination
       bndVar <- C.Var <$> C.freshVar "bnd" <*> pure NoLoc
-      tellPO
-        (conjunct (loopInvariant : guards))
-        (Bound (bndVar `C.gte` C.number 0) NoLoc)
-        (AtTermination loc)
+      tellPO (conjunct (loopInvariant : guards))
+             (Bound (bndVar `C.gte` C.number 0) NoLoc)
+             (AtTermination loc)
 
       -- bound decrement
       forM_ gdCmds $ \(GdCmd _ body) -> do
         let start = Bound (bnd `C.eqq` bndVar) loc
-        let pre = [start, extractAssertion body]
-        let post = Bound (bnd `C.lt` bndVar) NoLoc
+        let pre   = [start, extractAssertion body]
+        let post  = Bound (bnd `C.lt` bndVar) NoLoc
 
-        body' <- lift $ lift $ rewriteStruct pre body post
+        body' <- lift $ lift $ updateStruct pre body post
 
         genPO body'
 
@@ -202,14 +202,14 @@ runSpecM f = evalStateT (execWriterT f) 0
 
 
 genSpec :: Struct -> SpecM ()
-genSpec (Postcond _) = return ()
-genSpec (Struct _   []     next) = genSpec next
-genSpec (Struct pre (stmt:xs) next) = do
+genSpec (Postcond _                 ) = return ()
+genSpec (Struct _   []          next) = genSpec next
+genSpec (Struct pre (stmt : xs) next) = do
   case stmt of
-    Spec p q -> tellSpec p q
-    If _ gdCmds -> mapM_ (genSpec . gdCmdBody) gdCmds
+    Spec p q      -> tellSpec p q
+    If   _ gdCmds -> mapM_ (genSpec . gdCmdBody) gdCmds
     Do _ _ gdCmds -> mapM_ (genSpec . gdCmdBody) gdCmds
-    _ -> return ()
+    _             -> return ()
   genSpec (Struct pre xs next)
 
 
@@ -226,11 +226,11 @@ data StructError2 = MissingLoopInvariant Loc
 
 instance Located StructError2 where
   locOf (MissingLoopInvariant loc) = loc
-  locOf (MissingBound     loc) = loc
-  locOf (MissingPrecondition loc) = loc
+  locOf (MissingBound         loc) = loc
+  locOf (MissingPrecondition  loc) = loc
   locOf (MissingPostcondition loc) = loc
-  locOf (PreconditionUnknown loc) = loc
-  locOf (DigHole loc) = loc
+  locOf (PreconditionUnknown  loc) = loc
+  locOf (DigHole              loc) = loc
 
 instance ToJSON StructError2 where
 
@@ -239,11 +239,13 @@ instance ToJSON StructError2 where
 
 programToStruct :: C.Program -> WPM Struct
 programToStruct (C.Program _ stmts _) = case (init stmts, last stmts) of
-  (C.Assert          p l:stmts', C.Assert q m) -> wpStmts [Assertion p l] stmts' (Assertion q m)
-  (C.LoopInvariant p b l:stmts', C.Assert q m) -> wpStmts [LoopInvariant p b l] stmts' (Assertion q m)
-  ([]                          , C.Assert _ l) -> throwError (MissingPrecondition l)
-  (others               :_     , C.Assert _ _) -> throwError (MissingPrecondition (locOf others))
-  (_                           , stmt)         -> throwError (MissingPostcondition (locOf stmt))
+  (C.Assert p l : stmts', C.Assert q m) ->
+    wpStmts [Assertion p l] stmts' (Assertion q m)
+  (C.LoopInvariant p b l : stmts', C.Assert q m) ->
+    wpStmts [LoopInvariant p b l] stmts' (Assertion q m)
+  ([]        , C.Assert _ l) -> throwError (MissingPrecondition l)
+  (others : _, C.Assert _ _) -> throwError (MissingPrecondition (locOf others))
+  (_         , stmt        ) -> throwError (MissingPostcondition (locOf stmt))
 
 --------------------------------------------------------------------------------
 -- | Concrete Statements -> Struct
@@ -252,103 +254,106 @@ wpStmts :: [Pred] -> [C.Stmt] -> Pred -> WPM Struct
 wpStmts imposed stmts post = do
   accum <- wpStmts' imposed stmts post
   return $ fromAccum (conjunct $ reverse imposed) accum
-  where
-    wpStmts' :: [Pred] -> [C.Stmt] -> Pred -> WPM Accum
-    wpStmts' _       []           post = return (Accum [] (Postcond post))
-    wpStmts' imposed (stmt:stmts) post = case stmt of
-      C.Assert p l -> do
-        accum <- wpStmts' [Assertion p l] stmts post
-        return (Accum [] (fromAccum (Assertion p l) accum))
-      C.LoopInvariant p b l -> do
-        accum <- wpStmts' [LoopInvariant p b l] stmts post
-        return (Accum [] (fromAccum (LoopInvariant p b l) accum))
-      otherStmt             -> do
-        xs <- wpStmts' [] stmts post
-        x <- wp imposed otherStmt (precondAccum xs)
-        return $ insertAccum x xs
+ where
+  wpStmts' :: [Pred] -> [C.Stmt] -> Pred -> WPM Accum
+  wpStmts' _       []             post = return (Accum [] (Postcond post))
+  wpStmts' imposed (stmt : stmts) post = case stmt of
+    C.Assert p l -> do
+      accum <- wpStmts' [Assertion p l] stmts post
+      return (Accum [] (fromAccum (Assertion p l) accum))
+    C.LoopInvariant p b l -> do
+      accum <- wpStmts' [LoopInvariant p b l] stmts post
+      return (Accum [] (fromAccum (LoopInvariant p b l) accum))
+    otherStmt -> do
+      xs <- wpStmts' [] stmts post
+      x  <- wp imposed otherStmt (precondAccum xs)
+      return $ insertAccum x xs
 
 
-    wp :: [Pred] -> C.Stmt -> Pred -> WPM Stmt
-    wp imposed stmt post = case stmt of
-      C.Abort l              -> return $ Abort (L l (Constant C.false))
-      C.Skip l               -> return $ Skip (L l post)
-      C.Assert _ _           -> error "[ panic ] Assert in wp"
-      C.LoopInvariant _ _ _  -> error "[ panic ] LoopInvariant in wp"
-      C.Assign xs es l       -> do
-        pre <- subst (assignmentEnv xs es) post
-        return $ Assign (L l pre) xs es
+  wp :: [Pred] -> C.Stmt -> Pred -> WPM Stmt
+  wp imposed stmt post = case stmt of
+    C.Abort l               -> return $ Abort (L l (Constant C.false))
+    C.Skip  l               -> return $ Skip (L l post)
+    C.Assert _ _            -> error "[ panic ] Assert in wp"
+    C.LoopInvariant _  _  _ -> error "[ panic ] LoopInvariant in wp"
+    C.Assign        xs es l -> do
+      pre <- subst (assignmentEnv xs es) post
+      return $ Assign (L l pre) xs es
 
-      C.If gdCmds l -> do
+    C.If gdCmds l -> do
+      gdCmds' <- forM gdCmds $ \(C.GdCmd guard body m) -> do
+        let imposed' = guardIf guard : imposed
+        struct <- wpStmts imposed' body post
+        return $ GdCmd (GuardIf guard m) struct
+
+      let pre = disjunct (map guardIf (C.getGuards gdCmds))
+      return $ If (L l pre) gdCmds'
+
+    C.Do gdCmds l -> case imposed of
+      (LoopInvariant _ bnd _ : _) -> do
+
+        let loopInvariant = conjunct $ reverse imposed
+
+        -- use the loop invariant as the postcondition of the branch
         gdCmds' <- forM gdCmds $ \(C.GdCmd guard body m) -> do
-          let imposed' = guardIf guard : imposed
-          struct <- wpStmts imposed' body post
-          return $ GdCmd (GuardIf guard m) struct
+          let imposed' = guardLoop guard : imposed
+          struct <- wpStmts imposed' body loopInvariant
+          return $ GdCmd (GuardLoop guard m) struct
 
-        let pre = disjunct (map guardIf (C.getGuards gdCmds))
-        return $ If (L l pre) gdCmds'
+        return $ Do (L l loopInvariant) bnd gdCmds'
 
-      C.Do gdCmds l -> case imposed of
-        (LoopInvariant _ bnd _: _) -> do
+      _ -> throwError (MissingLoopInvariant l)
 
-          let loopInvariant = conjunct $ reverse imposed
-
-          -- use the loop invariant as the postcondition of the branch
-          gdCmds' <- forM gdCmds $ \(C.GdCmd guard body m) -> do
-            let imposed' = guardLoop guard : imposed
-            struct <- wpStmts imposed' body loopInvariant
-            return $ GdCmd (GuardLoop guard m) struct
-
-          return $ Do (L l loopInvariant) bnd gdCmds'
-
-        _ -> throwError (MissingLoopInvariant l)
-
-      C.SpecQM l            -> throwError (DigHole l)
-      C.Spec l              -> if null imposed
-        then return $ Spec (L l post) post
-        else return $ Spec (L l (conjunct (reverse imposed))) post
+    C.SpecQM l -> throwError (DigHole l)
+    C.Spec   l -> if null imposed
+      then return $ Spec (L l post) post
+      else return $ Spec (L l (conjunct (reverse imposed))) post
 
 
 --------------------------------------------------------------------------------
 -- | Struct -> Struct
 
--- rewrite the stored preconditions and postconditions of a struct
-rewriteStruct :: [Pred] -> Struct -> Pred -> WPM Struct
-rewriteStruct _       (Postcond _dumped)          post = return $ Postcond post
-rewriteStruct imposed (Struct _dumped stmts next) post = do
-  next' <- rewriteStruct [] next post
-  accum <- wpStmts' imposed stmts next'
+-- update the stored preconditions and postconditions of a struct
+updateStruct :: [Pred] -> Struct -> Pred -> WPM Struct
+updateStruct _       (Postcond _dumped         ) post = return $ Postcond post
+updateStruct imposed (Struct _dumped stmts next) post = do
+  next' <- updateStruct [] next post
+  accum <- updateAccum imposed stmts next'
   return $ fromAccum (conjunct $ reverse imposed) accum
 
-  where
-    wpStmts' :: [Pred] -> [Stmt] -> Struct -> WPM Accum
-    wpStmts' _       []           next = return $ Accum [] next
-    wpStmts' imposed (stmt:stmts) next = do
-      xs <- wpStmts' [] stmts next
-      x <- wp imposed stmt (precondAccum xs)
-      return $ insertAccum x xs
+ where
+  updateAccum :: [Pred] -> [Stmt] -> Struct -> WPM Accum
+  updateAccum _       []             next = return $ Accum [] next
+  updateAccum imposed (stmt : stmts) next = do
+    xs <- updateAccum [] stmts next
+    x  <- updateStmt imposed stmt (precondAccum xs)
+    return $ insertAccum x xs
 
-    wp :: [Pred] -> Stmt -> Pred -> WPM Stmt
-    wp imposed stmt post = case stmt of
-      Skip l -> return $ Skip (L (locOf l) post)
-      Abort l -> return $ Abort l
-      Assign l xs es -> do
-        pre <- subst (assignmentEnv xs es) post
-        return $ Assign (L (locOf l) pre) xs es
-      If l gdCmds -> do
-        gdCmds' <- forM gdCmds $ \(GdCmd guard body) -> do
-          let imposed' = guard : imposed
-          body' <- rewriteStruct imposed' body post
-          return $ GdCmd guard body'
-        return $ If l gdCmds'
-      Do (L l inv) bnd gdCmds -> do
-        gdCmds' <- forM gdCmds $ \(GdCmd guard body) -> do
-          let imposed' = guard : imposed
-          body' <- rewriteStruct imposed' body inv
-          return $ GdCmd guard body'
-        return $ Do (L l inv) bnd gdCmds'
-      Spec l _ -> if null imposed
-        then return $ Spec (L (locOf l) post) post
-        else return $ Spec (L (locOf l) (conjunct (reverse imposed))) post
+  updateStmt :: [Pred] -> Stmt -> Pred -> WPM Stmt
+  updateStmt imposed stmt post = case stmt of
+    Skip  l        -> return $ Skip (updateL l post)
+    Abort l        -> return $ Abort l
+    Assign l xs es -> do
+      pre <- subst (assignmentEnv xs es) post
+      return $ Assign (updateL l pre) xs es
+    If l gdCmds -> do
+      gdCmds' <- forM gdCmds $ \(GdCmd guard body) -> do
+        let imposed' = guard : imposed
+        body' <- updateStruct imposed' body post
+        return $ GdCmd guard body'
+      return $ If l gdCmds'
+    Do (L l inv) bnd gdCmds -> do
+      gdCmds' <- forM gdCmds $ \(GdCmd guard body) -> do
+        let imposed' = guard : imposed
+        body' <- updateStruct imposed' body inv
+        return $ GdCmd guard body'
+      return $ Do (L l inv) bnd gdCmds'
+    Spec l _ -> if null imposed
+      then return $ Spec (updateL l post) post
+      else return $ Spec (updateL l (conjunct (reverse imposed))) post
+
+  updateL :: L Pred -> Pred -> L Pred
+  updateL (L loc _) = L loc
 
 --------------------------------------------------------------------------------
 -- | Accum, temporary datatype for "incomplete" Structs
@@ -359,8 +364,8 @@ fromAccum :: Pred -> Accum -> Struct
 fromAccum pre (Accum xs next) = Struct pre xs next
 
 insertAccum :: Stmt -> Accum -> Accum
-insertAccum x (Accum xs ys) = Accum (x:xs) ys
+insertAccum x (Accum xs ys) = Accum (x : xs) ys
 
 precondAccum :: Accum -> Pred
-precondAccum (Accum []      xs) = extractAssertion xs
-precondAccum (Accum (stmt:_) _) = precond stmt
+precondAccum (Accum []         xs) = extractAssertion xs
+precondAccum (Accum (stmt : _) _ ) = precond stmt
