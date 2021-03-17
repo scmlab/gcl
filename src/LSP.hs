52b846055a2a5292e1ee943e353010e8ac0846ac
Ting-Gian LUA <banacorn@gmail.com>
2021-03-17 14:40:59 +0800

    [ change ] Remove ReqLoad

--- src/LSP.hs
+++ src/LSP.hs
@@ -159,37 +159,32 @@ handlers =
       notificationHandler STextDocumentDidSave $ \ntf -> do
         writeLog " --> TextDocumentDidSave"
         let NotificationMessage _ _ (DidSaveTextDocumentParams (TextDocumentIdentifier uri) text) = ntf
-
         case text of
+          Nothing -> pure ()
           Just source ->
             case uriToFilePath uri of
               Nothing -> pure ()
               Just filepath -> do
                 saveProgramSource source
-                response <- handleRequest (IdInt 0) (Req filepath source ReqLoad)
-                writeLog $ " <-- " <> pack (show response)
-                sendNotification (SCustomMethod "guacamole") $ JSON.toJSON response
-          Nothing -> pure (),
+                toLSPSideEffects filepath source,
       -- when the client opened the document
       notificationHandler STextDocumentDidOpen $ \ntf -> do
         writeLog " --> TextDocumentDidOpen"
         let NotificationMessage _ _ (DidOpenTextDocumentParams (TextDocumentItem uri _ _ source)) = ntf
+        saveProgramSource source
         case uriToFilePath uri of
           Nothing -> pure ()
-          Just filepath -> do
-            response <- handleRequest (IdInt 0) (Req filepath source ReqLoad)
-            writeLog $ " <-- " <> pack (show response)
-            sendNotification (SCustomMethod "guacamole") $ JSON.toJSON response
+          Just filepath -> toLSPSideEffects filepath source
     ]
 
 handleRequest :: ID -> Request -> LspT () ServerM Response
-handleRequest i request = do
+handleRequest i request@(Req filepath _) = do
   result <- getProgramSource
   case result of
     Nothing -> pure NotLoaded
     Just source -> do
       -- convert Request to LSP side effects
-      toLSPSideEffects i request
+      toLSPSideEffects filepath source
       -- convert Request to Response
       return $ toResponse i request source
 
@@ -198,15 +193,11 @@ handleRequest i request = do
 type ID = LspId ('CustomMethod :: Method 'FromClient 'Request)
 
 toResponse :: ID -> Request -> Text -> Response
-toResponse lspID (Req filepath _ kind) source =
+toResponse lspID (Req filepath kind) source =
   let responses = handle kind
    in Res filepath responses
   where
     handle :: ReqKind -> [ResKind]
-    handle ReqLoad = asGlobalError $ do
-      (pos, specs, globalProps) <- check filepath source
-
-      return [ResOK lspID pos specs globalProps]
     handle (ReqInspect selStart selEnd) = asGlobalError $ do
       (pos, specs, globalProps) <- check filepath source
       -- find the POs whose Range overlaps with the selection
@@ -242,108 +233,103 @@ toResponse lspID (Req filepath _ kind) source =
       return [ResConsoleLog "Export"]
     handle ReqDebug = error "crash!"
 
-toLSPSideEffects :: ID -> Request -> LspT () ServerM ()
-toLSPSideEffects _lspID (Req filepath source kind) = handle kind
+toLSPSideEffects :: FilePath -> Text -> LspT () ServerM ()
+toLSPSideEffects filepath source = do 
+  -- send diagnostics
+  diags <- do
+    let result = runM $ check filepath source
+    return $ case result of
+      Left err -> errorToDiagnostics err
+      Right (pos, _, _) -> map proofObligationToDiagnostic pos
+  let fileUri = toNormalizedUri (filePathToUri filepath)
+  let version = Just 0
+
+  publishDiagnostics 100 fileUri version (partitionBySource diags)
   where
-    handle :: ReqKind -> LspT () ServerM ()
-    handle ReqLoad = do
-      -- send diagnostics
-      diags <- do
-        let result = runM $ check filepath source
-        return $ case result of
-          Left err -> errorToDiagnostics err
-          Right (pos, _, _) -> map proofObligationToDiagnostic pos
-      let fileUri = toNormalizedUri (filePathToUri filepath)
-      let version = Just 0
-
-      publishDiagnostics 100 fileUri version (partitionBySource diags)
+    errorToDiagnostics :: Error -> [Diagnostic]
+    errorToDiagnostics (LexicalError pos) = [makeError (Loc pos pos) "Lexical error" ""]
+    errorToDiagnostics (SyntacticError errs) = map syntacticErrorToDiagnostics errs
       where
-        errorToDiagnostics :: Error -> [Diagnostic]
-        errorToDiagnostics (LexicalError pos) = [makeError (Loc pos pos) "Lexical error" ""]
-        errorToDiagnostics (SyntacticError errs) = map syntacticErrorToDiagnostics errs
-          where
-            syntacticErrorToDiagnostics (loc, msg) = makeError loc "Syntax error" (Text.pack msg)
-        errorToDiagnostics (StructError err) = structErrorToDiagnostics err
-          where
-            structErrorToDiagnostics (MissingAssertion loc) = [makeError loc "Assertion Missing" "Assertion before the DO construct is missing"]
-            structErrorToDiagnostics (MissingBound loc) = [makeError loc "Bound Missing" "Bound missing at the end of the assertion before the DO construct \" , bnd : ... }\""]
-            structErrorToDiagnostics (ExcessBound loc) = [makeError loc "Excess Bound" "Unnecessary bound annotation at this assertion"]
-            structErrorToDiagnostics (MissingPostcondition loc) = [makeError loc "Postcondition Missing" "The last statement of the program should be an assertion"]
-            structErrorToDiagnostics (DigHole _) = []
-        errorToDiagnostics (TypeError err) = typeErrorToDiagnostics err
-          where
-            typeErrorToDiagnostics (NotInScope name loc) = [makeError loc "Not in scope" $ "The definition " <> LazyText.toStrict name <> " is not in scope"]
-            typeErrorToDiagnostics (UnifyFailed s t loc) =
-              [ makeError loc "Cannot unify types" $
-                  renderStrict $
-                    "Cannot unify:" <+> pretty s <> line
-                      <> "with        :" <+> pretty t
-              ]
-            typeErrorToDiagnostics (RecursiveType var t loc) =
-              [ makeError loc "Recursive type variable" $
-                  renderStrict $
-                    "Recursive type variable:" <+> pretty var <> line
-                      <> "in type             :" <+> pretty t
-              ]
-            typeErrorToDiagnostics (NotFunction t loc) =
-              [ makeError loc "Not a function" $
-                  renderStrict $
-                    "The type" <+> pretty t <+> "is not a function type"
-              ]
-        errorToDiagnostics _ = []
+        syntacticErrorToDiagnostics (loc, msg) = makeError loc "Syntax error" (Text.pack msg)
+    errorToDiagnostics (StructError err) = structErrorToDiagnostics err
+      where
+        structErrorToDiagnostics (MissingAssertion loc) = [makeError loc "Assertion Missing" "Assertion before the DO construct is missing"]
+        structErrorToDiagnostics (MissingBound loc) = [makeError loc "Bound Missing" "Bound missing at the end of the assertion before the DO construct \" , bnd : ... }\""]
+        structErrorToDiagnostics (ExcessBound loc) = [makeError loc "Excess Bound" "Unnecessary bound annotation at this assertion"]
+        structErrorToDiagnostics (MissingPostcondition loc) = [makeError loc "Postcondition Missing" "The last statement of the program should be an assertion"]
+        structErrorToDiagnostics (DigHole _) = []
+    errorToDiagnostics (TypeError err) = typeErrorToDiagnostics err
+      where
+        typeErrorToDiagnostics (NotInScope name loc) = [makeError loc "Not in scope" $ "The definition " <> LazyText.toStrict name <> " is not in scope"]
+        typeErrorToDiagnostics (UnifyFailed s t loc) =
+          [ makeError loc "Cannot unify types" $
+              renderStrict $
+                "Cannot unify:" <+> pretty s <> line
+                  <> "with        :" <+> pretty t
+          ]
+        typeErrorToDiagnostics (RecursiveType var t loc) =
+          [ makeError loc "Recursive type variable" $
+              renderStrict $
+                "Recursive type variable:" <+> pretty var <> line
+                  <> "in type             :" <+> pretty t
+          ]
+        typeErrorToDiagnostics (NotFunction t loc) =
+          [ makeError loc "Not a function" $
+              renderStrict $
+                "The type" <+> pretty t <+> "is not a function type"
+          ]
+    errorToDiagnostics _ = []
 
-        proofObligationToDiagnostic :: PO -> Diagnostic
-        proofObligationToDiagnostic (PO _i _pre _post origin) = makeWarning loc title ""
-          where
-            -- we only mark the opening tokens ("do" and "if") for loops & conditionals
-            first2Char :: Loc -> Loc
-            first2Char NoLoc = NoLoc
-            first2Char (Loc start _) = Loc start (translate 1 start)
+    proofObligationToDiagnostic :: PO -> Diagnostic
+    proofObligationToDiagnostic (PO _i _pre _post origin) = makeWarning loc title ""
+      where
+        -- we only mark the opening tokens ("do" and "if") for loops & conditionals
+        first2Char :: Loc -> Loc
+        first2Char NoLoc = NoLoc
+        first2Char (Loc start _) = Loc start (translate 1 start)
 
-            loc :: Loc
-            loc = case origin of
-              -- we only mark the closing tokens ("od" and "fi") for loops & conditionals
-              AtLoop l -> first2Char l
-              AtTermination l -> first2Char l
-              AtIf l -> first2Char l
-              others -> locOf others
+        loc :: Loc
+        loc = case origin of
+          -- we only mark the closing tokens ("od" and "fi") for loops & conditionals
+          AtLoop l -> first2Char l
+          AtTermination l -> first2Char l
+          AtIf l -> first2Char l
+          others -> locOf others
 
-            title :: Text.Text
-            title = case origin of
-              AtAbort {} -> "Abort"
-              AtSpec {} -> "Spec"
-              AtAssignment {} -> "Assignment"
-              AtAssertion {} -> "Assertion"
-              AtIf {} -> "Conditional"
-              AtLoop {} -> "Loop Invariant"
-              AtTermination {} -> "Loop Termination"
-              AtSkip {} -> "Skip"
+        title :: Text.Text
+        title = case origin of
+          AtAbort {} -> "Abort"
+          AtSpec {} -> "Spec"
+          AtAssignment {} -> "Assignment"
+          AtAssertion {} -> "Assertion"
+          AtIf {} -> "Conditional"
+          AtLoop {} -> "Loop Invariant"
+          AtTermination {} -> "Loop Termination"
+          AtSkip {} -> "Skip"
 
-        -- translate a Pos along the same line
-        translate :: Int -> Pos -> Pos
-        translate n (Pos path ln col offset) = Pos path ln ((col + n) `max` 0) ((offset + n) `max` 0)
+    -- translate a Pos along the same line
+    translate :: Int -> Pos -> Pos
+    translate n (Pos path ln col offset) = Pos path ln ((col + n) `max` 0) ((offset + n) `max` 0)
 
-        posToPosition :: Pos -> Position
-        posToPosition (Pos _path ln col _offset) = Position ((ln - 1) `max` 0) ((col - 1) `max` 0)
+    posToPosition :: Pos -> Position
+    posToPosition (Pos _path ln col _offset) = Position ((ln - 1) `max` 0) ((col - 1) `max` 0)
 
-        locToRange :: Loc -> Range
-        locToRange NoLoc = Range (Position 0 0) (Position 0 0)
-        locToRange (Loc start end) = Range (posToPosition start) (posToPosition (translate 1 end))
+    locToRange :: Loc -> Range
+    locToRange NoLoc = Range (Position 0 0) (Position 0 0)
+    locToRange (Loc start end) = Range (posToPosition start) (posToPosition (translate 1 end))
 
-        locToLocation :: Loc -> Location
-        locToLocation NoLoc = Location (Uri "") (locToRange NoLoc)
-        locToLocation (Loc start end) = Location (Uri $ Text.pack $ posFile start) (locToRange (Loc start end))
+    locToLocation :: Loc -> Location
+    locToLocation NoLoc = Location (Uri "") (locToRange NoLoc)
+    locToLocation (Loc start end) = Location (Uri $ Text.pack $ posFile start) (locToRange (Loc start end))
 
-        severityToDiagnostic :: Maybe DiagnosticSeverity -> Loc -> Text.Text -> Text.Text -> Diagnostic
-        severityToDiagnostic severity loc title body = Diagnostic (locToRange loc) severity Nothing Nothing title Nothing (Just $ List [DiagnosticRelatedInformation (locToLocation loc) body])
+    severityToDiagnostic :: Maybe DiagnosticSeverity -> Loc -> Text.Text -> Text.Text -> Diagnostic
+    severityToDiagnostic severity loc title body = Diagnostic (locToRange loc) severity Nothing Nothing title Nothing (Just $ List [DiagnosticRelatedInformation (locToLocation loc) body])
 
-        makeWarning :: Loc -> Text.Text -> Text.Text -> Diagnostic
-        makeWarning = severityToDiagnostic (Just DsWarning)
+    makeWarning :: Loc -> Text.Text -> Text.Text -> Diagnostic
+    makeWarning = severityToDiagnostic (Just DsWarning)
 
-        makeError :: Loc -> Text.Text -> Text.Text -> Diagnostic
-        makeError = severityToDiagnostic (Just DsError)
-    handle ReqExportProofObligations = return ()
-    handle _ = pure ()
+    makeError :: Loc -> Text.Text -> Text.Text -> Diagnostic
+    makeError = severityToDiagnostic (Just DsError)
 
 --------------------------------------------------------------------------------
 
@@ -405,8 +391,7 @@ check filepath source = do
 
 -- | Request
 data ReqKind
-  = ReqLoad
-  | ReqInspect Int Int
+  = ReqInspect Int Int
   | ReqRefine Int Text
   | ReqSubstitute Int A.Expr A.Subst
   | ReqExportProofObligations
@@ -416,20 +401,19 @@ data ReqKind
 instance FromJSON ReqKind
 
 instance Show ReqKind where
-  show ReqLoad = "Load"
   show (ReqInspect x y) = "Inspect " <> show x <> " " <> show y
   show (ReqRefine i x) = "Refine #" <> show i <> " " <> show x
   show (ReqSubstitute i x y) = "Substitute #" <> show i <> " " <> show x <> " => " <> show y
   show ReqExportProofObligations = "ExportProofObligations"
   show ReqDebug = "Debug"
 
-data Request = Req FilePath Text ReqKind
+data Request = Req FilePath ReqKind
   deriving (Generic)
 
 instance FromJSON Request
 
 instance Show Request where
-  show (Req _path _content kind) = show kind
+  show (Req _path kind) = show kind
 
 --------------------------------------------------------------------------------
 
