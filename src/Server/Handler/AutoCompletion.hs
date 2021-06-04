b1edabee3c92972a48ceb2780bafcea6e293f90e
Ting-Gian LUA <banacorn@gmail.com>
2021-06-04 17:40:56 +0800

    [ refactor ] Move AutoCompletion handler out

---
+++ src/Server/Handler/AutoCompletion.hs
@@ -0,0 +1,109 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+{-# LANGUAGE TypeOperators #-}
+module Server.Handler.AutoCompletion where
+
+import           Language.LSP.Types
+import           Server.Interpreter.RealWorld
+
+handler :: Position -> Maybe CompletionContext -> ServerM (a |? CompletionList)
+handler position completionContext = do
+    -- only gets triggered when a backslash "\" is typed, or when the previous completion is incomplete
+    let
+        triggered = case completionContext of
+            (Just (CompletionContext CtTriggerCharacter (Just "\\"))) -> True
+            (Just (CompletionContext CtTriggerForIncompleteCompletions _)) ->
+                True
+            _ -> False
+    if triggered
+        then do
+            let Position ln col = position
+            let replaceRange    = Range (Position ln (col - 1)) position
+            let removeSlash     = Just $ List [TextEdit replaceRange ""]
+
+            let makeItem label kind symbol detail doc = CompletionItem
+                    label
+                    kind
+                    Nothing
+                    (Just detail)
+                    (Just $ CompletionDocString doc)
+                    Nothing
+                    Nothing
+                    Nothing
+                    Nothing
+                    (Just symbol)
+                    (Just PlainText)
+                    Nothing
+                    Nothing
+                    removeSlash
+                    Nothing
+                    Nothing
+                    Nothing
+
+            let items =
+                    [ makeItem "->"
+                               (Just CiOperator)
+                               "→"
+                               "\"→\" Rightwards Arrow"
+                               "The Unicode variant of \"->\""
+                    , makeItem "/="
+                               (Just CiOperator)
+                               "≠"
+                               "\"≠\" Not Equal To"
+                               "The Unicode variant of \"/=\""
+                    , makeItem ">="
+                               (Just CiOperator)
+                               "≥"
+                               "\"≥\" Greater-Than or Equal To"
+                               "The Unicode variant of \">=\""
+                    , makeItem "<="
+                               (Just CiOperator)
+                               "≤"
+                               "\"≤\" Less-Than or Equal To"
+                               "The Unicode variant of \"<=\""
+                    , makeItem "=>"
+                               (Just CiOperator)
+                               "⇒"
+                               "\"⇒\" Rightwards Double Arrow"
+                               "The Unicode variant of \"=>\""
+                    , makeItem "&&"
+                               (Just CiOperator)
+                               "∧"
+                               "\"∧\" Logical And"
+                               "The Unicode variant of \"&&\""
+                    , makeItem "||"
+                               (Just CiOperator)
+                               "∨"
+                               "\"∨\" Logical Or"
+                               "The Unicode variant of \"||\""
+                    , makeItem "~"
+                               (Just CiOperator)
+                               "¬"
+                               "\"¬\" Not Sign"
+                               "The Unicode variant of \"~\""
+                    , makeItem "<|"
+                               (Just CiValue)
+                               "⟨"
+                               "\"⟨\" Left Angle Bracket"
+                               "The Unicode variant of \"<|\""
+                    , makeItem "|>"
+                               (Just CiValue)
+                               "⟩"
+                               "\"⟩\" Right Angle Bracket"
+                               "The Unicode variant of \"|>\""
+                    , makeItem "min"
+                               (Just CiValue)
+                               "↓"
+                               "\"↓\" Downwards Arrow"
+                               "The Unicode variant of \"min\""
+                    , makeItem "max"
+                               (Just CiValue)
+                               "↑"
+                               "\"↑\" Upwards Arrow"
+                               "The Unicode variant of \"max\""
+                    ]
+
+            let isComplete     = True
+            let completionList = CompletionList isComplete (List items)
+            return $ InR completionList
+        else return $ InR $ CompletionList True (List [])
