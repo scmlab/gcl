274be2c83cd3ac327aab6713788e7f2ec1651e60
geoffcysu <geoffcysu@gmail.com>
2022-11-02 12:58:39 +0800

    Fixed renderPrec of Pred.

--- src/Render/Predicate.hs
+++ src/Render/Predicate.hs
@@ -2,7 +2,7 @@
 
 module Render.Predicate where
 
-import           Data.Loc                       ( locOf )
+import           Data.Loc                       ( locOf, Loc (NoLoc) )
 import           Data.Loc.Range                 ( fromLoc )
 import qualified Data.Text                     as Text
 import           GCL.Predicate
@@ -10,6 +10,8 @@ import           GCL.WP.Type
 import           Render.Class
 import           Render.Element
 import           Render.Syntax.Abstract         ( )
+import           Syntax.Abstract.Types          (Expr(..))
+import           Syntax.Common.Types (Op(..), ArithOp (..))
 
 instance Render StructWarning where
   render (MissingBound _)
@@ -34,16 +36,47 @@ instance RenderSection Spec where
     ]
 
 instance Render Pred where
-  renderPrec n x = case x of
-    Constant p          -> renderPrec n p
-    GuardIf   p _       -> renderPrec n p
-    GuardLoop p _       -> renderPrec n p
-    Assertion p _       -> renderPrec n p
-    LoopInvariant p _ _ -> renderPrec n p
-    Bound p _           -> renderPrec n p
-    Conjunct ps         -> punctuateE " ∧" (map render ps)
-    Disjunct ps         -> punctuateE " ∨" (map render ps)
-    Negate   p          -> "¬" <+> renderPrec n p
+  renderPrec n x = renderPrec n (exprOfPred x)
+  -- renderPrec n x = case x of
+  --   Constant p          -> renderPrec n p
+  --   GuardIf   p _       -> renderPrec n p
+  --   GuardLoop p _       -> renderPrec n p
+  --   Assertion p _       -> renderPrec n p
+  --   LoopInvariant p _ _ -> renderPrec n p
+  --   Bound p _           -> renderPrec n p
+  --   Conjunct ps         -> punctuateE " ∧" (map render ps)
+  --   Disjunct ps         -> punctuateE " ∨" (map render ps)
+  --   Negate   p          -> "¬" <+> renderPrec n p
+
+exprOfPred :: Pred -> Expr
+exprOfPred p = case p of
+  Constant ex -> ex
+  GuardIf ex _ -> ex
+  GuardLoop ex _ -> ex
+  Assertion ex _ -> ex
+  LoopInvariant ex _ _ -> ex
+  Bound ex _ -> ex
+  Conjunct prs -> case map exprOfPred prs of
+    []    -> error "Impossible case: incomplete Pred (in Render.Predicate.exprOfPred, Conjunct case)" 
+    [ex]  -> ex
+    x : y : exprs -> foldl (makeOpExpr conjOp)
+                           (makeOpExpr conjOp x y)
+                           exprs
+  Disjunct prs -> case map exprOfPred prs of
+    []    -> error "Impossible case: incomplete Pred (in Render.Predicate.exprOfPred, Disjunct case)"
+    [ex]  -> ex
+    x : y : exprs -> foldl (makeOpExpr disjOp)
+                           (makeOpExpr disjOp x y)
+                           exprs
+  Negate pr -> 
+    let ex = exprOfPred pr
+    in App (Op $ ArithOp $ NegU NoLoc) ex (locOf ex)
+  where
+    conjOp = Op (ArithOp (ConjU NoLoc))
+    disjOp = Op (ArithOp (DisjU NoLoc))
+    makeOpExpr :: Expr -> Expr -> Expr -> Expr
+    makeOpExpr op x y = App (App op x (locOf x)) y (locOf y)
+
 
 instance RenderSection PO where
   renderSection (PO pre post anchorHash anchorLoc origin) =
