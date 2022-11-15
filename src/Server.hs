adb97f51cf0b3f8924c4c64d58a977c7a4feb436
geoffcysu <geoffcysu@gmail.com>
2022-11-15 10:33:27 +0800

    Revert "[ server ] Changed default mode as socket mode."
    This reverts commit eb95434389473a9f99a6fd810fbc2c6bba07cfd6.

--- src/Server.hs
+++ src/Server.hs
@@ -33,15 +33,7 @@ run devMode = do
         _      <- runServerWithHandles handle handle (serverDefn env)
         putStrLn "== dev server closed =="
     else do
-      -- runServer (serverDefn env)
-      let port = "3000"
-      putStrLn "== GCL backend program started =="
-      _ <- forkIO (printLog env)
-      serve (Host "127.0.0.1") port $ \(sock, _remoteAddr) -> do
-        putStrLn $ "== GCL language server established at port:" ++ port ++ " =="
-        handle <- socketToHandle sock ReadWriteMode
-        _      <- runServerWithHandles handle handle (serverDefn env)
-        putStrLn "== GCL language server closed =="
+      runServer (serverDefn env)
  where
   printLog :: GlobalEnv -> IO ()
   printLog env = forever $ do
