3371b3b008764c70fc871a6dba111b1fc85d902c
Andy Shiue <andyshiue851019@gmail.com>
2024-03-01 17:27:59 +0800

    Prototypical HelloWorld implementation + refactoring.

--- src/Server/Handler2/CustomMethod.hs
+++ src/Server/Handler2/CustomMethod.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Server.Handler2.CustomMethod where
 
@@ -12,6 +13,8 @@ import Server.Monad (ServerM)
 import qualified Server.Monad (convertErrorsToResponsesAndDiagnostics)
 import Error (Error(..))
 
+import qualified Language.LSP.Server as LSP
+import qualified Language.LSP.Types  as LSP
 import Server.Handler2.Utils
 import qualified Server.Handler2.CustomMethod.Reload     as Reload (handler)
 import qualified Server.Handler2.CustomMethod.Inspect    as Inspect (handler)
@@ -22,7 +25,7 @@ import qualified Server.Handler2.CustomMethod.SubstituteRedex
                                                         as SubstituteRedex (handler)
 import qualified Server.Handler2.CustomMethod.HelloWorld as HelloWorld (handler)
 import qualified Data.Text as Text
-
+import Data.Aeson
 
 handler :: JSON.Value -> (Response -> ServerM ()) -> ServerM ()
 handler params responder = do
@@ -46,7 +49,18 @@ handler params responder = do
             ReqRefine2 range text             -> Refine.slowHandler range text respondResult reportError
             ReqInsertProofTemplate range hash -> InsertProofTemplate.slowHandler filePath range hash respondResult reportError
             ReqSubstitute redexNumber         -> SubstituteRedex.handler filePath redexNumber respondResult reportError
-            ReqHelloWorld range               -> HelloWorld.handler range respondResult reportError
+            ReqHelloWorld range               -> do
+              let requestParams =
+                    LSP.ShowMessageRequestParams
+                      LSP.MtInfo
+                      (Text.pack . show . encode $ ReqHelloWorld range)
+                      Nothing
+              _ <- LSP.sendRequest LSP.SWindowShowMessageRequest requestParams $ \case
+                  Right _ ->
+                    LSP.sendNotification LSP.SWindowShowMessage (LSP.ShowMessageParams LSP.MtInfo "Just saying hello again!")
+                  Left err ->
+                    LSP.sendNotification LSP.SWindowShowMessage (LSP.ShowMessageParams LSP.MtError $ "Something went wrong!\n" <> Text.pack (show err))
+              HelloWorld.handler range respondResult reportError
             _                                 -> reportError (Others "Not implemented yet.")
           where
             reportError :: Error -> ServerM ()
