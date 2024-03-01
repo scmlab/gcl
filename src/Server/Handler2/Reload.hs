98325694a3e4c3a474c587115788e13ac0bbea4c
Vince <vince.zhang@cellopoint.com>
2024-03-01 14:49:34 +0800

    fix(Server.Handler2.Reload): add missing imports

--- src/Server/Handler2/Reload.hs
+++ src/Server/Handler2/Reload.hs
@@ -1,8 +1,12 @@
 
+module Server.Handler2.Reload (handler) where
+
 import qualified Data.Aeson.Types as JSON
 import Server.Monad (ServerM)
+import Error (Error)
+import GCL.Predicate (PO (..), Spec (..))
 
-handler :: JSON.Value -> (Response -> ServerM ()) -> ServerM ()
+handler :: JSON.Value -> (Either Error ([Spec], [PO]) -> ServerM ()) -> ServerM ()
 handler params responder = do
     -- TODO:
     return ()
