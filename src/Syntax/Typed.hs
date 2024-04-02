d5cdaee4676949fe5f75dbba57965ffd82ce9f9d
Andy Shiue <andyshiue851019@gmail.com>
2024-04-02 14:53:27 +0800

    Add definition of typed syntax tree.

---
+++ src/Syntax/Typed.hs
@@ -0,0 +1,37 @@
+module Syntax.Typed where
+
+import Data.Text ( Text )
+import Data.Loc ( Loc )
+import Data.Loc.Range ( Range )
+import Syntax.Abstract.Types ( Lit, Type )
+import Syntax.Common.Types ( Name, Op )
+
+data TypedProgram = Program [()] -- definitions (the functional language part)
+                       [()] -- constant and variable declarations
+                       [()] -- global properties
+                       [TypedStmt] -- main program
+                       Loc
+
+data TypedStmt
+  = Skip Type Loc
+  | Abort Loc
+  | Assign [Name] [TypedExpr] Loc
+  | AAssign TypedExpr TypedExpr TypedExpr Loc
+  | Assert TypedExpr Loc
+  | LoopInvariant TypedExpr TypedExpr Loc
+  | Do [TypedGdCmd] Loc
+  | If [TypedGdCmd] Loc
+  | Spec Text Range
+  | Proof Text Text Range
+  -- TODO: Other constructors.
+
+data TypedGdCmd = TypedGdCmd TypedExpr [TypedStmt] Loc
+
+data TypedExpr
+  = Lit Lit Type Loc
+  | Var Name Type Loc
+  | Const Name Type Loc
+  | Op Op Type
+  | App TypedExpr TypedExpr Loc
+  | Lam Name Type TypedExpr Loc
+  -- TODO: Other constructors.
